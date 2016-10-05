babd6e02bfca58a2d9192b5e7abdef6733cd43ff
shangjianwei <shangjianwei159@163.com>
2016-10-05 11:48:08 +0800

    尝试

--- LingLongManHua/app/src/main/java/com/sjw/linglongmanhua/fragment/FindFragment.java
+++ LingLongManHua/app/src/main/java/com/sjw/linglongmanhua/fragment/FindFragment.java
@@ -6,8 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.sjw.linglongmanhua.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,7 +30,15 @@ public class FindFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_find, container, false);
+        View inflate = inflater.inflate(R.layout.fragment_find, container, false);
+        ListView listView= (ListView) inflate.findViewById(R.id.listview_test_find);
+        List<String> list=new ArrayList<>();
+        for (int i = 0; i <100; i++) {
+           list.add("aaa"+i);
+        }
+        ArrayAdapter arrayAdapter=new ArrayAdapter(getActivity(),android.R.layout.simple_list_item_1,android.R.id.text1,list);
+        listView.setAdapter(arrayAdapter);
+        return inflate;
     }
 
 }
